d4bb17bd8caf31b495b22565e26a656ff95545c4
Gunnar Van Remoortere <gunnarvanremoortere@hotmail.com>
2021-05-22 22:01:48 +0200

    created models and installed mongoose

--- controllers/api/v1/transfers.js
+++ controllers/api/v1/transfers.js
@@ -1,19 +1,40 @@
+const Transfer = require('../../../models/Transfer');
+
 const getAll = (req, res) => {
-    res.json({
-        "status": "success",
-        "data": {
-            "todos": []
+    Transfer.find({"sender": "Gunnar"}, (err, docs) => {
+        if(!err){
+            res.json({
+                "status": "success",
+                "data": {
+                    "transfers": docs
+                }
+            });
         }
     });
+
 }
 
 const create = (req, res) => {
-    res.json({
-        "status": "success",
-        "data": {
-            "todo": {
-                "text": "learn Node.js"
-            }
+    console.log(req.body);
+    let transfer = new Transfer();
+    transfer.sender = req.body.sender;
+    transfer.recipient = req.body.recipient;
+    transfer.message = req.body.message;
+    transfer.amount = req.body.amount;
+    transfer.save((err, doc) => {
+       if (err) {
+           res.json({
+               "status": "error",
+               "message": "Could not save this transfer"
+           });
+       }
+        if (!err) {
+            res.json({
+                "status": "success",
+                "data": {
+                    "transfer": doc
+                }
+            });
         }
     });
 }
