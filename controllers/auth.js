28240562cb11645fe42ad478e2588e877299be64
Gunnar Van Remoortere <gunnarvanremoortere@hotmail.com>
2021-05-22 23:34:18 +0200

    register updated with first and lastname

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,10 +2,11 @@ const User = require('../models/User');
 const passport = require('../passport/passport');
 
 const signup = async (req, res, next) => {
-    let username = req.body.username;
+    let firstname = req.body.firstname;
+    let lastname = req.body.lastname;
     let password = req.body.password;
 
-    const user = new User({username: username});
+    const user = new User({firstname: firstname, lastname: lastname});
     await user.setPassword(password);
     await user.save().then(result => {
         res.json({
@@ -19,7 +20,7 @@ const signup = async (req, res, next) => {
 };
 
 const login = async  (req, res, next) => {
-    const user = await User.authenticate()(req.body.username, req.body.password)
+    const user = await User.authenticate()(req.body.firstname, req.body.lastname, req.body.password)
         .then(result => {
             res.json({
                 "status": "success",
