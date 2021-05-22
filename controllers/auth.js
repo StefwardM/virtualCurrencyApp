15735b5e4a037ffd217493f087fefaa096aa02b7
Stefan Mladjenovic <stefanmladjenovic@hotmail.com>
2021-05-23 00:33:01 +0200

    Users now have to sign up with a thomasmore email account and they start with 100 coins

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,40 +1,50 @@
 const User = require('../models/User');
-const passport = require('../passport/passport');
 
 const signup = async (req, res, next) => {
-    let username = req.body.username;
+    let username = req.body.username; // UI of postman
+    let password = req.body.password;
     let firstname = req.body.firstname;
     let lastname = req.body.lastname;
-    let password = req.body.password;
+    let coins = 100;
 
-    const user = new User({username: username, firstname: firstname, lastname: lastname});
-    await user.setPassword(password);
-    await user.save().then(result => {
+    if(username.includes("@student.thomasmore.be")) {
+        const user = new User({
+            username: username,
+            firstname: firstname,
+            lastname: lastname,
+            coins: coins
+        });
+
+        await user.setPassword(password);
+        await user.save().then(result => {
+            res.json({
+                "status": "success"
+            })
+        }).catch(error => {
+            res.json({
+                "status": "error"
+            })
+        });
+    } else {
+        throw "email must include @student.thomasmore.be";
+    }
+
+};
+
+const login = async (req, res, next) => {
+    const user = await User.authenticate()(req.body.username, req.body.password).then(result => {
         res.json({
-            "status": "success"
+            "status": "success",
+            "data": {
+                "user": result
+            }
         })
     }).catch(error => {
         res.json({
-            "status": "error"
+            "status": "error",
+            "message": error
         })
     });
-};
-
-const login = async  (req, res, next) => {
-    const user = await User.authenticate()(req.body.username, req.body.password)
-        .then(result => {
-            res.json({
-                "status": "success",
-                "data": {
-                    "user": result
-                }
-            });
-        }).catch(error => {
-            res.json({
-                "status": "error",
-                "message": error
-            });
-        });
 };
 
 module.exports.signup = signup;
