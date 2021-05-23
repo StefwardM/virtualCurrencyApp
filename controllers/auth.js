3232ff1fe89b09007bcb7dead7ed1b75180ba01f
Gunnar Van Remoortere <gunnarvanremoortere@hotmail.com>
2021-05-23 16:07:09 +0200

    made login function work and transfers are reachable in the frontend

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const passport = require('../passport/passport');
+const jwt = require('jsonwebtoken');
 
 const signup = async (req, res, next) => {
     let username = req.body.username;
@@ -12,8 +13,18 @@ const signup = async (req, res, next) => {
         const user = new User({username: username, firstname: firstname, lastname: lastname, coins: coins});
         await user.setPassword(password);
         await user.save().then(result => {
+            console.log(result._id, result.username, result.firstname, result.lastname);
+            let token = jwt.sign({
+                uid: result._id,
+                username: result.username,
+                firstname: result.firstname,
+                lastname: result.lastname
+            }, "MyVerySecretWord");
             res.json({
-                "status": "success"
+                "status": "success",
+                "data": {
+                    "token": token
+                }
             })
         }).catch(error => {
             res.json({
@@ -30,10 +41,22 @@ const signup = async (req, res, next) => {
 const login = async  (req, res, next) => {
     const user = await User.authenticate()(req.body.username, req.body.password)
         .then(result => {
-            res.json({
+            if (!result.user){
+                return res.json({
+                    "status": "failed",
+                    "message": "Login failed"
+                })
+            }
+
+            let token = jwt.sign({
+                uid: result.user._id,
+                username: result.user.username
+            }, "MyVerySecretWord");
+
+            return res.json({
                 "status": "success",
                 "data": {
-                    "user": result
+                    "token": token
                 }
             });
         }).catch(error => {
